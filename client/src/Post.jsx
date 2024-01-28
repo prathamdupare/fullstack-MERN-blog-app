fc99ab9a7d61a0b324a9ca09503e7688808649d0
Pratham Dupare <108093728+prathamdupare@users.noreply.github.com>
2024-01-28 12:25:16 +0530

    Update Post.jsx

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -10,8 +10,8 @@ export default function Post({
   author,
 }) {
   return (
-    <div className="entry flex p-4 border rounded-lg m-2 hover:bg-gray-100">
-      <div className="w-1/3 mr-4">
+    <div className="entry flex p-2 mx-8 border rounded-lg m-2 hover:bg-gray-100">
+      <div className="w-1/4 mr-4">
         <Link to={`post/${_id}`}>
           <img
             className="w-full h-auto object-cover rounded-lg"
@@ -20,20 +20,20 @@ export default function Post({
           />
         </Link>
       </div>
-      <div className="flex-1 flex flex-col justify-between">
+      <div className="flex-1 flex flex-col ">
         <div>
           <Link
             to={`post/${_id}`}
-            className="font-semibold text-lg text-blue-600 hover:underline"
+            className="font-semibold text-base text-blue-600 hover:underline"
           >
             {title}
           </Link>
-          <p className="text-sm text-gray-500 mt-1">{summary}</p>
+          <p className="text-xs w-[75%] text-gray-500 mt-1">{summary}</p>
         </div>
         <div className="flex items-center mt-2">
-          <p className="text-sm text-gray-600">{author.username}</p>
+          <p className="text-xs text-gray-600">{author.username}</p>
           <span className="mx-2 text-gray-400">•</span>
-          <time className="text-sm text-gray-600">
+          <time className="text-xs text-gray-600">
             {formatISO9075(new Date(createdAt))}
           </time>
         </div>
